69eb549baebd7e46f1847e8c8fefcc7b25fed789
329517313@qq.com <329517313@qq.com>
2017-03-31 16:10:20 +0800

    增加了Dog类

---
+++ meituan/meituan/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  meituan
+//
+//  Created by Nic Tang on 2017/3/31.
+//  Copyright © 2017年 Nic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ meituan/meituan/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  meituan
+//
+//  Created by Nic Tang on 2017/3/31.
+//  Copyright © 2017年 Nic. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
